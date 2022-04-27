0f16a3f55653b7ee51729b89b0dd069b9367419a
GoranAviani <goranaviani@gmail.com>
2022-04-27 21:05:20 +0200

    react_hooks_in_practice

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,51 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
 
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
+const App = () => {
+  const [state, setState] = useState({ videos: [], selectedVideo: null });
 
-  componentDidMount() {
-    this.onTermSubmit("buildings");
-  }
+  useEffect(()=>onTermSubmit("buildings"),[])
 
-  onTermSubmit = async (term) => {
+  const onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
         q: term,
       },
     });
 
-    this.setState({
+    setState({
       videos: response.data.items,
       selectedVideo: response.data.items[0],
     });
   };
 
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
+  const onVideoSelect = (video) => {
+    setState({ selectedVideo: video });
   };
 
-  render() {
+
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <SearchBar onFormSubmit={onTermSubmit} />
         <div className="ui grid">
           <div className="ui row">
             <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
+              <VideoDetail video={state.selectedVideo} />
             </div>
             <div className="five wide column">
               <VideoList
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}
+                onVideoSelect={onVideoSelect}
+                videos={state.videos}
               />
             </div>
           </div>
         </div>
       </div>
     );
-  }
+
 }
 
 export default App;
